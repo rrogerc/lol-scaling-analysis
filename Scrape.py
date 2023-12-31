239f041f6c1677d2819e59fa48ae252bc5528932
Roger Chen <rogerchen803@gmail.com>
2023-12-30 19:11:09 -0700

    Allow to scrape by role

--- Scrape.py
+++ Scrape.py
@@ -6,20 +6,23 @@ from Champions import champions, champion_numbers
 all_champions_data = {}
 json_filename = "diamond_plus.json"
 tier = "diamond_plus"
+role = "bottom"  # default, top, jungle, middle, bottom, support
 session = requests.Session()
 
 for i in range(champions.__len__()):
-    data_url = f"https://ax.lolalytics.com/mega/?ep=champion&p=d&v=1&patch=30&cid={champion_numbers[i]}&lane=default&tier={tier}&queue=420&region=all"
+    data_url = f"https://ax.lolalytics.com/mega/?ep=champion&p=d&v=1&patch=30&cid={champion_numbers[i]}&lane={role}&tier={tier}&queue=420&region=all"
     
     response = session.get(data_url)
 
     if response.status_code != 200:
         print("Failed to retrieve data. Status code: {response.status_code}: " + champions[i])
         continue
-
     print(f"Processing: {champions[i]} + {champion_numbers[i]}")
 
     data = response.json()
+
+    if role != "default" and data["nav"]["lanes"][role] <= 7:
+        continue
 
     # Extract 'timeWin' and 'time' data and calculate winrates
     time_win_data = data["timeWin"]
